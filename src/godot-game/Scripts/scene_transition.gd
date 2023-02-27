79ae9b107b2b2477321c311b93a7945bcd9ed4ae
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-02-27 12:46:56 -0300

    VER 0.1.13.1
    Recomentar scene_transition.gd (tava mto poluido)
    Mudar animation smoothing do player (agr ta mais rapido)
    Atualizar versao para 0.1.3.1

--- src/godot-game/Scripts/scene_transition.gd
+++ src/godot-game/Scripts/scene_transition.gd
@@ -1,37 +1,16 @@
-# Este script é uma extensão do CanvasLayer, que é um nó que representa uma camada de canvas,
-# responsável por desenhar elementos em um espaço 2D.
-
 extends CanvasLayer
 
-# A variável warnFix é inicializada com None, indicando que ainda não possui um valor atribuído.
-
-var warnFix = null
-
-# A função change_scene recebe uma string como parâmetro, que representa o nome da cena que
-# deseja-se transicionar. A função retorna None.
-
+# Função que recebe uma string (caminho da cena a ser trocada) e não retorna nada
 func change_scene(target: String) -> void:
-	
-	# A função play do nó AnimationPlayer é chamada, com o parâmetro 'dissolve', que
-	# representa a animação de transição entre as cenas.
-	
+	# Acessa o nó AnimationPlayer e executa a animação 'dissolve'
 	$AnimationPlayer.play('dissolve')
-	
-	# O código é pausado até que a animação atual seja finalizada.
-	
+	# Pausa a execução da função até que a animação seja finalizada
 	yield($AnimationPlayer, 'animation_finished')
 	
-	# A função get_tree() é chamada, que retorna o nó SceneTree, responsável pela gerência
-	# da cena atual e das transições entre cenas. A função change_scene é chamada, passando
-	# como parâmetro a string recebida na função change_scene.
-	
-	warnFix = get_tree().change_scene(target);
-	
-	# A função play_backwards do nó AnimationPlayer é chamada, com o parâmetro 'dissolve', que
-	# representa a animação inversa da transição entre as cenas.
-	
-	$AnimationPlayer.play_backwards('dissolve')
-	
-	# A palavra-chave 'pass' é utilizada para finalizar a função.
+	# Tenta mudar para a cena especificada, caso ocorra um erro, imprime mensagem
+	if get_tree().change_scene(target) != OK:
+		print ("An unexpected error occured when trying to switch to the scene")
 
+	# Executa a animação 'dissolve' no sentido contrário, "desfazendo" a animação anterior
+	$AnimationPlayer.play_backwards('dissolve')
 	pass
