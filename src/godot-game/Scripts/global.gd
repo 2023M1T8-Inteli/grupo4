ff775dac2f1936515c07e09b020c0eb50da92d30
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-03-03 07:36:37 -0300

    VER 0.1.5.3
    Inicio da add da parte administrativa (Thomas)
    Implementacao da task quiz (NAO FUNCIONA AINDA)

--- src/godot-game/Scripts/global.gd
+++ src/godot-game/Scripts/global.gd
@@ -4,3 +4,9 @@ var leftPress = false
 var rightPress = false
 var upPress = false
 var downPress = false
+
+var startQuiz1 = false
+var correctBoxQuiz1
+var correctAnswerQuiz1 = false
+var jumpStartQuiz1 = false
+var jumpStartFeedback1 = false
