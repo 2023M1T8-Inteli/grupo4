f243380e4cc44cd4a1298a3c2b67cc342f771342
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-03-09 13:46:46 -0300

    VER 0.2.2.0
    Mudar controles (agora eh MUITO BOOOM UAAAAU)

--- src/godot-game/Scripts/Player.gd
+++ src/godot-game/Scripts/Player.gd
@@ -7,8 +7,10 @@ onready var arrow = $Arrow
 var amplitude = 60
 var frequency = 5
 
+
 var vel : Vector2 = Vector2()
 
+var target_position: Vector2
 
 #Atribuindo nossa sprite como variavel
 onready var spriteA : AnimatedSprite = get_node("Fantasma/Ghost")
@@ -25,6 +27,7 @@ onready var rightPress = Global.rightPress
 onready var upPress = Global.upPress
 onready var downPress = Global.downPress
 
+
 func _process(_delta):
 	if pos.savePosCommand == true:
 		pos.savePosCommand = false
@@ -34,7 +37,7 @@ func _process(_delta):
 		
 	
 	arrow.active_objective = Global.activeObjective[1]
-	if self.global_position.distance_to(Global.activeObjective[1]) < 200:
+	if self.global_position.distance_to(Global.activeObjective[1]) < 180:
 		$Arrow.visible = false
 	else:
 		$Arrow.visible = true
@@ -79,66 +82,18 @@ func _on_DownButton_button_down():
 
 	
 #física do jogo
-func _physics_process(_delta):
-	if Global.isDrunk == true:
-		var time = OS.get_ticks_msec() / 1000.0
-		var x_offset = amplitude * sin(time * frequency)
-		var y_offset = amplitude * cos(time * frequency)
-		var speed = Vector2(450, 450)
-		var direction = Vector2()
-		if Input.is_action_pressed("move_left") or Global.leftPress == true:
-			direction.x -= 1
-			leftPress = true
-		else:
-			leftPress = false
-		if Input.is_action_pressed("move_right") or Global.rightPress == true:
-			direction.x += 1
-			rightPress = true
-		else:
-			rightPress = false
-		if Input.is_action_pressed("move_up") or Global.upPress == true:
-			direction.y -= 1
-			upPress = true
-		else:
-			upPress = false
-		if Input.is_action_pressed("move_down") or Global.downPress == true:
-			direction.y += 1
-			downPress = true
-		else:
-			downPress = false
-		direction = direction.normalized()
-		if leftPress or downPress or upPress or rightPress:
-			var velocity = direction * speed + Vector2(x_offset, y_offset)
-			vel = move_and_slide(velocity, Vector2.UP)
-		_player_dir()
+func _physics_process(delta):
+	if Input.is_action_pressed("touch"):
+		target_position = get_viewport().get_mouse_position()
+		var angle = atan2(target_position.x - 180, target_position.y - 320)
+		var x = sin(angle) * 1
+		var y = cos(angle) * 1
+		var velocity = Vector2(x*speed, y*speed)
+		move_and_slide(velocity, Vector2.UP)
+		_player_dir(velocity)
 	else:
-		var speed = Vector2(450, 450) 
-		var direction = Vector2()
-		if Input.is_action_pressed("move_left") or Global.leftPress == true:
-			direction.x -= 1
-			leftPress = true
-		else:
-			leftPress = false
-		if Input.is_action_pressed("move_right") or Global.rightPress == true:
-			direction.x += 1
-			rightPress = true
-		else:
-			rightPress = false
-		if Input.is_action_pressed("move_up") or Global.upPress == true:
-			direction.y -= 1
-			upPress = true
-		else:
-			upPress = false
-		if Input.is_action_pressed("move_down") or Global.downPress == true:
-			direction.y += 1
-			downPress = true
-		else:
-			downPress = false
-		direction = direction.normalized()
-		vel = direction * speed
-		vel = move_and_slide(vel, Vector2.UP)
+		_player_dir(Vector2(0,0))
 	
-		_player_dir()
 
 
 #Funcao responsavel por controlar que direcao o player esta olhando enquanto/apos se mexer
@@ -155,50 +110,19 @@ func idle_sprites(show_idle: bool):
 	zeIdle.visible = show_idle
 	zeCorre.visible = not show_idle
 
-func _player_dir():
-	# Variáveis para armazenar a direção do jogador
-	var fLeft = false
-	var fRight = false
-	var fUp = false
-	var fDown = false
-
+func _player_dir(velocity):
 	# Verifica se o jogador está parado
-	if leftPress == false and rightPress == false and upPress == false and downPress == false:
+	if velocity == Vector2(0,0):
 		idle_sprites(true)
 	else:
 		idle_sprites(false)
 	
 	# Faz o jogador olhar para a direção que está se movendo
-	if leftPress ==  true:
-		fLeft = true
-		fRight = false
-	elif rightPress == true:
-		fRight = true
-		fLeft = false
-	
-	# Mostra a sprite das costas quando o jogador se move verticalmente para baixo
-	if downPress == true:
-		fDown = true
-		fUp = false
-	elif upPress == true:
-		# Mostra a sprite da frente quando o jogador se move verticalmente para cima
-		spriteA.visible = true
-		spriteB.visible = false
-		fUp = true
-		fDown = false
-		
-	# Volta a sprite para a posição normal dependendo da direção em que o jogador está se movendo
-	if fLeft == true:
+	if velocity.x < 0:
 		flip_sprites(true)
-	if fRight == true:
+	elif velocity.x > 0:
 		flip_sprites(false)
-	if fDown == true:
-		spriteA.visible = false
-		spriteB.visible = true
-	if fUp == true:
-		spriteA.visible = true
-		spriteB.visible = false
-		
+
 
 
 
