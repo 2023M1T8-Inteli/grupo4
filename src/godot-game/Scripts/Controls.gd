cba17ed5a041cd419a7488395d4ed8ee24ad9c48
izadoraluz <izadora.novaes@sou.inteli.edu.br>
2023-03-13 15:16:32 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/2023M1T8-Inteli/grupo4""
    This reverts commit a810f684ed689042dbba7e23bd54c5b93e79188e.

--- src/godot-game/Scripts/Controls.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-# Define a função que será chamada quando o botão "BackButton" for pressionado
-func _on_BackButton_pressed() -> void:
-	# Obtém a árvore de cenas e muda para a cena "Title Screen.tscn" quando o botão é pressionado
-	if get_tree().change_scene("res://Scenes/Title Screen.tscn") != OK:
-		print ("An unexpected error occured when trying to switch to the scene")
