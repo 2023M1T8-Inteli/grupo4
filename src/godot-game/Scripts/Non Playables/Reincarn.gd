052805aed6c13c0d02b7140f4c6b658fd13d2000
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-03-18 00:10:33 -0300

    VER 0.3.2.3
    * Adicionar Limbo4
    * Exportar para gitpages

--- src/godot-game/Scripts/Non Playables/Reincarn.gd
+++ src/godot-game/Scripts/Non Playables/Reincarn.gd
@@ -5,4 +5,6 @@ func _on_TextureButton_pressed():
 	if Global.parte == "administrativo":
 		SceneTransition.change_scene("res://Scenes/Playables/Environment/Limbo3.tscn", 1, 1)
 	elif Global.parte == "tecnico":
-		SceneTransition.change_scene("res://Scenes/Non Playables/misc/Title Screen.tscn", 1.5 , 1.5)
+		SceneTransition.change_scene("res://Scenes/Playables/Environment/Limbo4.tscn", 1, 1)
+	elif Global.parte == "fim":
+		SceneTransition.change_scene("res://Scenes/Non Playables/misc/Title Screen.tscn", 1.5, 1.5)
---
+++ src/godot-game/Scripts/Playables/Limbo4.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+func _ready():
+	# RESETAR POSICAO DEFAULT CIDADE
+	pos.posCidade = Vector2(368, 624)
+	
+	# Define variáveis globais para controle do jogo
+	Global.canMove = false  # Indica se o jogador pode se mover
+	Global.isDrunk = false  # Indica se o jogador está bêbado
+	$LimboAmbience.playing = true  # Toca o barulho de fundo do limbo
+	$LimboMusic.playing = true  # Toca a música do limbo
+	
+	# Espera 3.3 segundos antes de exibir a caixa de diálogo
+	yield(get_tree().create_timer(3.3), "timeout")
+	$"DialogBox 10".visible = true  # Exibe a caixa de diálogo
+	$"DialogBox 10/TexturaCaixa"._startDialog()  # Inicia a animação da caixa de diálogo
+	
+	# Conecta o sinal "finish" da textura da caixa de diálogo a esta cena
+	$"DialogBox 10/TexturaCaixa".connect("finish", self, "_on_TexturaCaixa_finish")
+	$QuizTask.connect("quizFinish", self, "_quiz1_finish")
+	$QuizTask2.connect("quizFinish", self, "_quiz2_finish")
+	$"DialogBox 11/TexturaCaixa".connect("finish", self, "_on_TexturaCaixa2_finish")
+# Função chamada quando a animação da caixa de diálogo termina
+func _on_TexturaCaixa_finish():
+	$QuizTask.visible = true
+	$QuizTask._startQuiz()
+
+func _quiz1_finish():
+	$QuizTask.visible = false
+	$QuizTask2.visible = true
+	$QuizTask2._startQuiz()
+	
+func _quiz2_finish():
+	$"DialogBox 11".visible = true
+	$"DialogBox 11/TexturaCaixa"._startDialog() 
+	
+func _on_TexturaCaixa2_finish():
+	Global.parte = "fim"
+	#SceneTransition.change_scene("res://Scenes/Playables/Environment/Cidade.tscn", 1, 1)
+	SceneTransition.change_scene("res://Scenes/Non Playables/misc/Reincarn.tscn", 1, 1)
